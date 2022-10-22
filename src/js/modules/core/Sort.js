6c6225ffec26dd0b7bd2986f19d4554bb7e48d15
ryuga.sasaki <ryuga.private@gmail.com>
2022-10-22 16:35:14 +0900

    added selectionSort algorithm

--- src/js/modules/core/Sort.js
+++ src/js/modules/core/Sort.js
@@ -6,7 +6,7 @@ export default class Sort {
    * @param {number[]} array - 未整列の配列
    * @return {number[]} 整列済みの配列
    */
-  static quick(array) {
+  static quickSort(array) {
     // 配列の値が一つの場合はそのまま返す
     if (1 >= array.length) return array;
 
@@ -20,5 +20,48 @@ export default class Sort {
     const larger = others.filter((x) => x > pivot);
 
     return [...this.quick(smallerOrEqual), pivot, ...this.quick(larger)];
+  }
+
+  /**
+   * バブルソート (基本交換法)
+   * @param {number[]} array - 未整列の配列
+   * @returns {number[]} 整列済みの配列
+   */
+  static bubbleSort(array) {
+    // 引数の配列のクローンを作成
+    const clone = Array.from(array);
+    const compareNumbers = (a, b) => {
+      return a - b;
+    };
+    return clone.sort(compareNumbers);
+  }
+
+  /**
+   * セレクションソート (基本選択法)
+   * @param {number[]} array - 未整列の配列
+   * @returns {number[]} 整列済みの配列
+   */
+  static selectionSort(array) {
+    // 引数の配列のクローンを作成
+    const clone = Array.from(array);
+
+    const length = clone.length; // 配列の要素数
+
+    let temp = 0;
+
+    for (let i = 0; i < length - 1; i++) {
+      let min = i; // 最小値
+      for (let j = i + 1; j < length; j++) {
+        if (clone[min] > clone[j]) {
+          min = j;
+        }
+      }
+      // 入れ替え
+      temp = clone[min];
+      clone[min] = clone[i];
+      clone[i] = temp;
+    }
+
+    return clone;
   }
 }
